510c2f22ffc658d1d92b4b42e61e41292e3a4a59
abhutan1 <aayubhutani@gmail.com>
2020-04-12 16:58:56 -0700

    Add toggle functionality for slots polling and provide more user options

--- background.js
+++ background.js
@@ -1,6 +1,47 @@
+var delivery_slots_script_running;
+const TIME_IN_60_SECONDS = 60 * 1000
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"clicked": true});
-  });
-});
+    if (activeTab.url.includes("https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html")){
+      if (delivery_slots_script_running){
+        alert("Hello there\n\nI am going to stop looking for delivery slots now. Toggle the icon if you want me to try again.\n\nBye!")
+        delivery_slots_script_running = false
+        return
+      }
+      var find_delivery_slots = confirm("Hello there\n\nLooks like you are in need of groceries. I can help you out by finding delivery slots. Just click OK and I will send a notification when I find a slot.\n\nIf you do not want my help, hit Cancel")
+      if (find_delivery_slots){
+        delivery_slots_script_running = true
+        chrome.tabs.executeScript({
+          file: 'delivery_slots.js'
+        })
+      }
+    }
+    else{
+      alert("Sorry :( Looks like this is not the correct page. I cannot help you unless you are on the Schedule Your Delivery page")
+    }
+  })
+})
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.message === "incorrect_page") {
+      delivery_slots_script_running = false
+      alert("Sorry :( Looks like this is not the correct page. I cannot help you unless you are on the Schedule Your Delivery page")
+    }
+    else if (request.message === "has_delivery_slot") {
+      delivery_slots_script_running = false
+      alert("YAY! I found a delivery slot for you! Come back and take it before it goes away.")
+    }
+    else if (request.message === "correct_page_but_no_delivery_slot" && delivery_slots_script_running) {
+      setTimeout(() =>
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          var activeTab = tabs[0];
+          chrome.tabs.executeScript({
+            file: 'delivery_slots.js'
+          })
+      }), TIME_IN_60_SECONDS)
+    }
+  }
+)
--- delivery_slots.js
+++ delivery_slots.js
@@ -28,36 +28,20 @@ function hasDeliverySlots(schedule_widget) {
 // This function checks if the user is on the correct page.
 // If no, it alerts the user and asks to go back to the correct page
 // If yes, it reloads the webpage and tries to find delivery slots.
-// If a delivery slot is found, stops the timer and sends a notification
+// If a delivery slot is found, sends a notification
 function retryHasDeliverySlots(){
+  window.location.reload(true)
   var schedule_widget = document.getElementsByClassName("a-container ufss-widget-container")
   if (!IsScheduleDeliveryPage(schedule_widget)){
-    alert("Sorry :( Looks like this is not the correct page. I cannot help you unless I am on the Schedule Your Delivery page")
+    chrome.runtime.sendMessage({"message": "incorrect_page"})
   }
 
   if (!hasDeliverySlots(schedule_widget)){
-    window.location.reload(true)
+    chrome.runtime.sendMessage({"message": "has_delivery_slot"})
   }
   else{
-    alert("YAY! I found a delivery slot for you! Come back and take it before it goes away.")
+    chrome.runtime.sendMessage({"message": "correct_page_but_no_delivery_slot"})
   }
 }
 
-// Controls how often the function tries to find delivery slots
-const TIME_IN_60_SECONDS = 60 * 1000
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.clicked === true) {
-      var schedule_widget = document.getElementsByClassName("a-container ufss-widget-container")
-      if (!IsScheduleDeliveryPage(schedule_widget)){
-        alert("Sorry :( Looks like this is not the correct page. I cannot help you unless I am on the Schedule Your Delivery page")
-      }
-      else{
-        var find_delivery_slots = confirm("Hello there\n\nLooks like you are in need of groceries. I can help you out by finding delivery slots. Just click OK and I will send a notification when I find a slot.\n\nIf you do not want my help, hit Cancel")
-        if (find_delivery_slots){
-          setTimeout(() => retryHasDeliverySlots(), TIME_IN_60_SECONDS);
-        }
-      }
-    }
-  }
-)
+retryHasDeliverySlots()
