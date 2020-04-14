5926bff8e072de926ed87cdeec4ec188cc570ddf
abhutan1 <aayubhutani@gmail.com>
2020-04-13 18:58:50 -0700

    Code cleanup in background.js

--- background.js
+++ background.js
@@ -6,46 +6,45 @@ const TIME_IN_5_SECONDS = 5 * 1000
 const TIME_IN_60_SECONDS = 60 * 1000
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    if (notify_sound_on){
-      notify_sound_on = false
-      clearInterval(notify_sound_interval)
-      alert("Hope you were able to secure a delivery spot.\n\nStay home! Stay safe!")
+  if (notify_sound_on){
+    notify_sound_on = false
+    clearInterval(notify_sound_interval)
+    alert("Hope you were able to secure a delivery spot.\n\nStay home! Stay safe!")
+    return
+  }
+
+  if (delivery_slots_script_running){
+    delivery_slots_script_running = false
+    alert("Hello there\n\nI am going to stop looking for delivery slots now. " +
+          "Toggle the icon if you want me to try again.\n\nBye!")
+    return
+  }
+
+  if (tab.url.includes("https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html")){
+    var find_delivery_slots = confirm("Hello there :)\n\nLooks like you are in " +
+                                      "need of groceries. I can help you out " +
+                                      "by finding delivery slots.\n\nJust click " +
+                                      "OK and I will alert you when " +
+                                      "I find a slot.\n\nBEFORE CLICKING OK, "+
+                                      "MAKE SURE THAT THE VOLUME IS SET TO LOUD."+
+                                      "\n\nI suggest that you minimize this window "+
+                                      "and open a new one to browse the web. "+
+                                      "Toggle me at any point "+
+                                      "to enable or disable things that I do."+
+                                      "\n\nIf you do not want my help, hit Cancel.")
+    if (find_delivery_slots){
+      delivery_slots_script_running = true
+      chrome.tabs.executeScript({
+        file: 'delivery_slots.js'
+      })
       return
     }
-
-    if (delivery_slots_script_running){
-      delivery_slots_script_running = false
-      alert("Hello there\n\nI am going to stop looking for delivery slots now. " +
-            "Toggle the icon if you want me to try again.\n\nBye!")
-      return
-    }
-
-    if (tabs[0].url.includes("https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html")){
-      var find_delivery_slots = confirm("Hello there :)\n\nLooks like you are in " +
-                                        "need of groceries. I can help you out " +
-                                        "by finding delivery slots.\n\nJust click " +
-                                        "OK and I will alert you when " +
-                                        "I find a slot.\n\nBEFORE CLICKING OK, "+
-                                        "MAKE SURE THAT THE VOLUME IS SET TO LOUD."+
-                                        "\n\nI suggest that you minimize this window "+
-                                        "and open a new one to browse the web. "+
-                                        "Toggle me at any point "+
-                                        "to enable or disable things that I do."+
-                                        "\n\nIf you do not want my help, hit Cancel.")
-      if (find_delivery_slots){
-        delivery_slots_script_running = true
-        chrome.tabs.executeScript({
-          file: 'delivery_slots.js'
-        })
-      }
-    }
-    else{
-      alert("Sorry :(\n\nLooks like this is not the correct page. I can only start "+
-            "looking for slots if you toggle me from the Schedule Your Order page.\n\n"+
-             "Go here to see how it looks like: https://github.com/abhutan1/Amazon-Whole-Foods-Delivery-Slots-Finder/blob/master/Schedule_Your_Order_Page.png")
-    }
-  })
+  }
+  else{
+    alert("Sorry :(\n\nLooks like this is not the correct page. I can only start "+
+          "looking for slots if you toggle me from the Schedule Your Order page.\n\n"+
+           "Go here to see how it looks like: https://github.com/abhutan1/Amazon-Whole-Foods-Delivery-Slots-Finder/blob/master/Schedule_Your_Order_Page.png")
+  }
 })
 
 chrome.runtime.onMessage.addListener(
@@ -56,31 +55,28 @@ chrome.runtime.onMessage.addListener(
 
     if (request.clicked === "checkout_wfm_cart"){
       setTimeout(() =>
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.executeScript({
-            file: 'before_you_checkout.js'
-          })
-      }), TIME_IN_5_SECONDS)
+        chrome.tabs.executeScript({
+          file: 'before_you_checkout.js'
+        })
+      , TIME_IN_5_SECONDS)
       return
     }
 
     if (request.clicked === "before_you_checkout"){
       setTimeout(() =>
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.executeScript({
-            file: 'subs_prefs.js'
-          })
-      }), TIME_IN_5_SECONDS)
+        chrome.tabs.executeScript({
+          file: 'subs_prefs.js'
+        })
+      , TIME_IN_5_SECONDS)
       return
     }
 
     if (request.clicked === "subs_prefs"){
       setTimeout(() =>
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.executeScript({
-            file: 'delivery_slots.js'
-          })
-      }), TIME_IN_5_SECONDS)
+        chrome.tabs.executeScript({
+          file: 'delivery_slots.js'
+        })
+      , TIME_IN_5_SECONDS)
       return
     }
 
@@ -89,11 +85,10 @@ chrome.runtime.onMessage.addListener(
         chrome.tabs.update(tabs[0].id, {url: "https://www.amazon.com/cart/localmarket"})
       })
       setTimeout(() =>
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.executeScript({
+        chrome.tabs.executeScript({
             file: 'checkout_wfm_cart.js'
-          })
-      }), TIME_IN_5_SECONDS)
+        })
+      , TIME_IN_5_SECONDS)
       return
     }
 
@@ -109,11 +104,11 @@ chrome.runtime.onMessage.addListener(
 
     if (request.message === "correct_page_but_no_delivery_slot") {
       setTimeout(() =>
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.executeScript({
+        chrome.tabs.executeScript({
             file: 'delivery_slots.js'
-          })
-      }), TIME_IN_60_SECONDS)
+        })
+      , TIME_IN_60_SECONDS)
+      return
     }
   }
 )
